2db15a659932c965993ee85f885b5d01a653934c
MD KAVIUL HOSSAIN <kaviuln@gmail.com>
2022-09-08 12:16:44 +1000

    Update

--- test/test.js
+++ test/test.js
@@ -9,67 +9,57 @@ describe("Add Two Numbers", function() {
             done()
           });
     });
-//});
-  
-    it("returns statusCode key in body to check if api give right result should be 300", function(done) {
-      request(url, function(error, response, body) {
-           body = JSON.parse(body)
-             expect(body.statusCode).to.equal(300);
-             done()
-            });      
-     });
-
-     it("should return status 200", function(done) {
+    it("returns statusCode key in body to check if api give right result should be 200", function(done) {
         request(url, function(error, response, body) {
             expect(response.statusCode).to.equal(200);
             done()
           });
     });
-
-   // });
-  //   it("returns the result as number", function(done) {
-  //       request(url, function(error, response, body) {
-  //           body = JSON.parse(body)
-  //           expect(body.result).to.be.a('number');
-  //           done()
-  //         });
-  //    });
-  //  // })
-  //   it("returns the result equal to 8", function(done) {
-  //     request(url, function(error, response, body) {
-  //         body = JSON.parse(body)
-  //         expect(body.result).to.equal(8);
-  //         done()
-  //       });
-  // });
-//});
-//   it("returns the result not equal to 15", function(done) {
-//     request(url, function(error, response, body) {
-//         body = JSON.parse(body)
-//         expect(body.result).to.not.equal(15);
-//         done()
-//       });
-// });
+    it("returns the result as number", function(done) {
+        request(url, function(error, response, body) {
+            body = JSON.parse(body)
+            expect(body.result).to.be.a('number');
+            done()
+          });
+    });
+    it("returns the result equal to 8", function(done) {
+      request(url, function(error, response, body) {
+          body = JSON.parse(body)
+          expect(body.result).to.equal(8);
+          done()
+        });
+  });
+  it("returns the result not equal to 15", function(done) {
+    request(url, function(error, response, body) {
+        body = JSON.parse(body)
+        expect(body.result).to.not.equal(15);
+        done()
+      });
+});
   });
 
-  
-    
-//});
-    // it("returns statusCode key in body to check if api gives right result should be 200", function(done) {
-    //     request(url, function(error, response, body) {
-    //         body = JSON.parse(body)
-    //         expect(body.statusCode).to.equal(200);
-    //         done()
-    //       });
-    //   });
-    // it("returns the result as null", function(done) {
-    //     request(url, function(error, response, body) {
-    //         body = JSON.parse(body)
-    //         expect(body.result).to.be.a('null');
-    //         done()
-    //       });
-    // });
-  //});
+  describe("Add Two strings", function() {
+    var url = "http://localhost:8080/addTwoNumbers/a/b";
+    it("should return status 200", function(done) {
+        request(url, function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done()
+          });
+    });
+    it("returns statusCode key in body to check if api gives right result should be 200", function(done) {
+        request(url, function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done()
+          });
+    });
+    it("returns the result as null", function(done) {
+        request(url, function(error, response, body) {
+            body = JSON.parse(body)
+            expect(body.result).to.be.a('null');
+            done()
+          });
+    });
+  });
 
 
   describe("Add Two strings", function() {
@@ -82,9 +72,8 @@ describe("Add Two Numbers", function() {
     });
     it("returns the result as array", function(done) {
         request(url, function(error, response, body) {
-            body = JSON.parse(body)
-            expect(body.data).to.be.an('array');
+            expect(response.statusCode).to.be.a('array');
             done()
           });
-     });
+    });
   });
